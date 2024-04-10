090c373c5d8040ae97379646081c4ba973e2cfde
paizay24 <paizayoo.dev2024@gmail.com>
2024-04-10 21:51:54 +0630

    frontend ready

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.php",'node_modules/preline/dist/*.js',],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin'),
+  ],
+}
+

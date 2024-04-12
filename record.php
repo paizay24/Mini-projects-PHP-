2c7267d28f84d436da665f960f3e60852fe07d6d
paizay24 <paizayoo.dev2024@gmail.com>
2024-04-12 21:42:43 +0630

    record added in file

--- record.php
+++ record.php
@@ -1,14 +1,3 @@
-<?php
-
-if (empty($_POST['ur_width']) || empty($_POST['ur_height'])) {
-      header('Location:index.php');
-}
-
-
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -169,8 +158,17 @@ if (empty($_POST['ur_width']) || empty($_POST['ur_height'])) {
 
                         </li>
                         <li class="inline-flex items-center text-sm font-semibold text-gray-800 truncate dark:text-gray-200" aria-current="page">
-                              <a class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500" href="#">
+                              <a class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500" href="./area.php">
                                     Result
+                              </a>
+                              <svg class="flex-shrink-0 mx-2 overflow-visible size-4 text-gray-400 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <path d="m9 18 6-6-6-6"></path>
+                              </svg>
+
+                        </li>
+                        <li class="inline-flex items-center text-sm font-semibold text-gray-800 truncate dark:text-gray-200" aria-current="page">
+                              <a class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500" href="#">
+                                    Records
                               </a>
                               <svg class="flex-shrink-0 mx-2 overflow-visible size-4 text-gray-400 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                     <path d="m9 18 6-6-6-6"></path>
@@ -180,29 +178,28 @@ if (empty($_POST['ur_width']) || empty($_POST['ur_height'])) {
                   </ol>
                   <h1 class=" text-center font-serif font-bold m-4 text-3xl">Area Calculator</h1>
                   <?php
-                  $height = $_POST['ur_height'];
-                  $width = $_POST['ur_width'];
 
-                  $area = $height * $width;
 
                   $fileName = "saveRecord.txt";
                   if (file_exists($fileName)) {
                         touch($fileName);
                   };
 
-                  $fileStream = fopen($fileName, 'a');
-                  fwrite($fileStream, "\n$width * $height = $area");
+                  $fileStream = fopen($fileName, 'r');
+                  while (!feof($fileStream)) : {
+                              $content = fgets($fileStream);
+                              // if($content === '/n') continue;
+                        }
 
-                  fclose($fileStream);
 
                   ?>
-                  <p class=" text-center text-2xl font-mono font-bold"><?= $area . ' ' . 'sqft' ?></p>
-                  <a href="./index.php">
-                        <button type="button" class=" mt-3 py-3 w-full justify-center px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
-                              Calculate Again
-                        </button>
-                        
-                  </a>
+
+                        <p class=" font-mono text-gray-700 bg-white p-4 rounded-md mb-2">
+                              <?= $content; ?>
+                        </p>
+
+                  <?php endwhile; ?>
+
             </section>
       </main>
 
